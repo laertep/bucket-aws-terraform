71315f4d5d0ab6aae2eecbdad7fbd9cc2cf462a8
Laerte Porto Júnior <44216245+laertep@users.noreply.github.com>
2022-12-14 22:40:48 -0300

    Update provider

--- provider.tf
+++ provider.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region  = "us-east-1"
-  access_key = "AKIA5YCGQBPO3J7NQCGB"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 
